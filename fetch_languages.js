8d4c5d45806bbb1a0ea2c33c7ede0c5ce7f8780f
tomoyahiroe <tomoya4creative@gmail.com>
2022-08-12 20:24:18 +0900

    usernameを指定した状態での必要な変数の取得に成功

--- fetch_languages.js
+++ fetch_languages.js
@@ -1,3 +1,5 @@
+//ユーザのpublicレポジトリに含まれる言語の数を得るファイル
+
 import { graphql } from "@octokit/graphql";
 const token = process.env.GIT_EXTENSION_TOKEN;
 
@@ -35,29 +37,31 @@ query getUser {
 }
 `;
 
-(async function getInfo() {
+export async function getKindsOfLanguage(username) {
   try {
-    const { user } = await graphqlWithAuth(QUERY, { login: "tomoyahiroe" });
+    const { user } = await graphqlWithAuth(QUERY, { login: username });
     // console.log(user);
     return user;
   } catch (err) {
     console.error(err);
   }
-})().then((data) => {
-  const jsonData = JSON.stringify(data, null, 2);
-  // console.log(jsonData);
-  let languageArray = [];
-  const nodes = data.repositories.edges;
-  console.log(nodes.length);
-  for (let i = 0; i < nodes.length; i++) {
-    const node = nodes[i];
-    if (node.node.primaryLanguage === null) return;
-    const usedLanguage = node.node.primaryLanguage.name;
-    if (languageArray.indexOf(usedLanguage) === 1) return;
-    languageArray.push(usedLanguage);
-  }
-  console.log("結果は" + languageArray);
-  return languageArray;
-
-  //jsonオブジェクトから、情報を抜き取る。
-});
+  // }.then((data) => {
+  //   const jsonData = JSON.stringify(data, null, 2);
+  //   // console.log(jsonData);
+  //   let languageArray = [];
+  //   const nodes = data.repositories.edges;
+  //   // console.log(nodes);
+  //   for (let i = 0; i < nodes.length; i++) {
+  //     const node = nodes[i];
+  //     if (node.node.primaryLanguage !== null) {
+  //       const usedLanguage = node.node.primaryLanguage.name;
+  //       if (languageArray.indexOf(usedLanguage) === -1) {
+  //         languageArray.push(usedLanguage);
+  //       }
+  //     }
+  //   }
+  //   // console.log(languageArray);
+  //   return languageArray.length; //フレームワークも1言語とする。
+  // });
+}
+// exports = { getKindsOfLanguage };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { graphql } from "@octokit/graphql";
 import { createObjectCsvWriter } from "csv-writer";
+import { getKindsOfLanguage } from "./fetch_languages.js";
 const token = process.env.GIT_EXTENSION_TOKEN;
 
 const graphqlWithAuth = graphql.defaults({
@@ -9,8 +10,8 @@ const graphqlWithAuth = graphql.defaults({
 });
 
 const QUERY = `
-query getUser {
-  user(login: "tomoyahiroe") {
+query getUser($login: String!) {
+  user(login: $login) {
     login
     name
     contributionsCollection(to: "2021-12-31T09:23:59", from: "2021-01-01T09:00:00") {
@@ -23,32 +24,119 @@ query getUser {
 }
 `;
 
-(async function getInfo() {
+async function getInfo(username) {
   try {
-    const { user } = await graphqlWithAuth(QUERY, { login: "tomoyahiroe" });
+    const { user } = await graphqlWithAuth(QUERY, { login: username });
     // console.log(user);
     return user;
   } catch (err) {
     console.error(err);
   }
-})().then((data) => {
-  const jsonData = JSON.stringify(data, null, 2);
-  console.log(jsonData);
+}
+// .then((data) => {
+//   const jsonData = JSON.stringify(data, null, 2);
+//   console.log(jsonData);
 
-  //jsonオブジェクトから、情報を抜き取る。
-  const username = data.login;
-  const sumContributions =
-    data.contributionsCollection.contributionCalendar.totalContributions;
-  const amountOfRepository =
-    data.contributionsCollection.totalRepositoryContributions;
-  console.log({
-    username: username,
-    amountOfRepository: amountOfRepository,
-    sumContributions: sumContributions,
+//   //jsonオブジェクトから、情報を抜き取る。
+//   const username = data.login;
+//   const sumContributions =
+//     data.contributionsCollection.contributionCalendar.totalContributions;
+//   const amountOfRepository =
+//     data.contributionsCollection.totalRepositoryContributions;
+//   console.log({
+//     username: username,
+//     amountOfRepository: amountOfRepository,
+//     sumContributions: sumContributions,
+//   });
+//   return {
+//     username: username,
+//     amountOfRepository: amountOfRepository,
+//     sumContributions: sumContributions,
+//   };
+// })
+// .then((info) => {
+//   const kinddsOfLanguage = getKindsOfLanguage(info.username).then((data) => {
+//     const jsonData = JSON.stringify(data, null, 2);
+//     // console.log(jsonData);
+//     let languageArray = [];
+//     const nodes = data.repositories.edges;
+//     // console.log(nodes);
+//     for (let i = 0; i < nodes.length; i++) {
+//       const node = nodes[i];
+//       if (node.node.primaryLanguage !== null) {
+//         const usedLanguage = node.node.primaryLanguage.name;
+//         if (languageArray.indexOf(usedLanguage) === -1) {
+//           languageArray.push(usedLanguage);
+//         }
+//       }
+//     }
+//     // console.log(languageArray);
+//     return languageArray.length; //フレームワークも1言語とする。
+//   });
+//   return {
+//     username: info.username,
+//     amountOfRepository: info.amountOfRepository,
+//     sumContributions: info.sumContributions,
+//     kinddsOfLanguage: kinddsOfLanguage,
+//   };
+// });
+const username = "shion1305";
+const row_data = getInfo(username)
+  .then((data) => {
+    const jsonData = JSON.stringify(data, null, 2);
+    console.log(jsonData);
+
+    //jsonオブジェクトから、情報を抜き取る。
+    const username = data.login;
+    const sumContributions =
+      data.contributionsCollection.contributionCalendar.totalContributions;
+    const amountOfRepository =
+      data.contributionsCollection.totalRepositoryContributions;
+    console.log({
+      username: username,
+      amountOfRepository: amountOfRepository,
+      sumContributions: sumContributions,
+    });
+    return {
+      username: username,
+      amountOfRepository: amountOfRepository,
+      sumContributions: sumContributions,
+    };
+  })
+  .then((info) => {
+    getKindsOfLanguage(info.username)
+      .then((data) => {
+        const jsonData = JSON.stringify(data, null, 2);
+        // console.log(jsonData);
+        let languageArray = [];
+        const nodes = data.repositories.edges;
+        // console.log(nodes);
+        for (let i = 0; i < nodes.length; i++) {
+          const node = nodes[i];
+          if (node.node.primaryLanguage !== null) {
+            const usedLanguage = node.node.primaryLanguage.name;
+            if (languageArray.indexOf(usedLanguage) === -1) {
+              languageArray.push(usedLanguage);
+            }
+          }
+        }
+        // console.log(languageArray);
+        return languageArray.length; //フレームワークも1言語とする。
+      })
+      .then((kindsOfLanguage) => {
+        console.log({
+          username: info.username,
+          amountOfRepository: info.amountOfRepository,
+          sumContributions: info.sumContributions,
+          kindsOfLanguage: kindsOfLanguage,
+        });
+        return {
+          username: info.username,
+          amountOfRepository: info.amountOfRepository,
+          sumContributions: info.sumContributions,
+          kindsOfLanguage: kindsOfLanguage,
+        };
+      });
   });
-  return {
-    username: username,
-    amountOfRepository: amountOfRepository,
-    sumContributions: sumContributions,
-  };
-});
+
+// console.log(row_data);
